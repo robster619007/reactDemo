be4bd68f2bd201d0e989fdddb04372d04c74efca
Robee Ghosh <robeeghosh@Robees-MacBook-Pro.local>
2020-03-05 19:45:46 +0400

    Updates

---
+++ precache-manifest.edc9a59c402c0c85d405a1c44ccc6a38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42bf1a2ae141252234e502437e469df6",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "de5b02a39ecc155bb1ce",
+    "url": "/me/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "de5b02a39ecc155bb1ce",
+    "url": "/me/static/js/2.a7231ffc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/me/static/js/2.a7231ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b070fe93c20a78fd9c95",
+    "url": "/me/static/js/main.e7ad4afe.chunk.js"
+  },
+  {
+    "revision": "31d3860e35e166713ebf",
+    "url": "/me/static/js/runtime-main.7eb173c1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactDemo/precache-manifest.be82fb8864a045e320b59357887ee916.js"
+  "/me/precache-manifest.edc9a59c402c0c85d405a1c44ccc6a38.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactDemo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/me/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
